19d16c6445bc0b68048dd1b556ab5246537db592
robyn <robyn@agrigateone.com>
2025-04-09 15:26:45 +0200

    Update SQL scripts

--- scripts/carton_groupings.sql
+++ scripts/carton_groupings.sql
@@ -1,52 +1,93 @@
 SELECT 
-    carton_groupings.id, 
-    carton_groupings.line_item_id,
-    carton_groupings.order_id,
-    carton_groupings.state,
+    carton_groupings.id,
     carton_groupings.pallet_number,
-    carton_groupings.sequence_number,
-    carton_groupings.exporter_code,
-    carton_groupings.farm_code,
-    carton_groupings.packhouse_code,
+    carton_groupings.container_number,
+    carton_groupings.seller_id,
+    carton_groupings.buyer_id,
+    carton_groupings.packing_week,
     production_regions.name production_region,
+    carton_groupings.orchard,
     commodities.name commodity_name,
     varieties.name variety_name,
-    carton_groupings.cartons,
+    carton_groupings.map_size_count size_count,
+    CASE 
+        WHEN carton_groupings.size_count ~ '^\d+(\.0+)?$' and commodities.name <> 'Soft Citrus' THEN
+        CASE
+            -- Lemon sizing
+            WHEN LOWER(commodities.name) = 'lemon' THEN
+                CASE
+                    WHEN carton_groupings.size_count::numeric::int >= 189 THEN 'Extra Small'
+                    WHEN carton_groupings.size_count::numeric::int >= 138 THEN 'Small'
+                    WHEN carton_groupings.size_count::numeric::int >= 100 THEN 'Medium'
+                    WHEN carton_groupings.size_count::numeric::int >= 75 THEN 'Large'
+                    ELSE 'Large'
+                END
+
+            -- Valencia oranges and similar
+            WHEN LOWER(commodities.name) = 'orange' THEN
+                CASE
+                    WHEN carton_groupings.size_count::numeric::int <= 48 THEN 'Large'
+                    WHEN carton_groupings.size_count::numeric::int <= 64 THEN 'Medium'
+                    WHEN carton_groupings.size_count::numeric::int <= 88 THEN 'Small'
+                    WHEN carton_groupings.size_count::numeric::int <= 125 THEN 'Extra Small'
+                    ELSE 'Mixed/Non-standard'
+                END
+
+            -- Star Ruby grapefruit sizing
+            WHEN LOWER(commodities.name) = 'grapefruit' THEN
+                CASE
+                    WHEN carton_groupings.size_count::numeric::int <= 35 THEN 'Large'
+                    WHEN carton_groupings.size_count::numeric::int <= 45 THEN 'Medium'
+                    WHEN carton_groupings.size_count::numeric::int <= 55 THEN 'Small'
+                    WHEN carton_groupings.size_count::numeric::int = 60 THEN 'Extra Small'
+                    ELSE 'Mixed/Non-standard'
+                END
+
+            -- Default for numeric but uncategorized commodity
+            ELSE 'Unknown'
+        END
+
+    -- Non-numeric soft citrus codes and overrides
+    ELSE
+        CASE
+            WHEN LOWER(commodities.name) = 'soft citrus' THEN
+                CASE
+                    WHEN carton_groupings.size_count ILIKE ANY (ARRAY['1XXX', '1XX']) THEN 'Large'
+                    WHEN carton_groupings.size_count ILIKE ANY (ARRAY['1X', '1', '1.0']) THEN 'Medium'
+                    WHEN carton_groupings.size_count IN ('2', '3', '2.0', '3.0') THEN 'Small'
+                    WHEN carton_groupings.size_count IN ('4', '5', '6', '4.0', '5.0', '6.0') THEN 'Extra Small'
+                    ELSE 'Mixed/Non-standard'
+                END
+
+            -- Literal character groupings and fallback mappings
+            WHEN carton_groupings.size_count ILIKE 'M' THEN 'Medium'
+            WHEN carton_groupings.size_count ILIKE 'L' THEN 'Large'
+            WHEN carton_groupings.size_count ILIKE 'XL' THEN 'Large'
+            WHEN carton_groupings.size_count ILIKE 'XS' THEN 'Extra Small'
+			WHEN carton_groupings.size_count ILIKE 'S' THEN 'Small'
+            -- Known invalid/mixed inputs
+            WHEN carton_groupings.size_count ~* 'mix|juice|juic|var|waste|xx|lm|ms|mx|/' THEN 'Mixed/Non-standard'
+            ELSE 'Unknown'
+        END
+    END AS size_categorization,
+    carton_groupings.map_class class,
     carton_groupings.pallet_stack,
     carton_groupings.pack,
-    carton_groupings.size_count,
-    carton_groupings.grade,
-    carton_groupings.orchard,
-    carton_groupings.net_mass,
-    carton_groupings.container_number,
-    carton_groupings.mark,
     carton_groupings.map_local_market local_market,
     carton_groupings.map_jbin jbin,
     carton_groupings.target_market,
     carton_groupings.target_region,
     carton_groupings.target_country,
-    carton_groupings.pallet_gross_mass,
-    carton_groupings.seller_id,
-    cg_sellers.types seller_types,
-    carton_groupings.buyer_id,
-    cg_buyers.types buyer_types,
-    carton_groupings.packing_week,
-    carton_groupings.batch_number,
-    carton_groupings.inventory_code,
-    carton_groupings.consignment_number,
-    carton_groupings.pallet_rejected,
-    carton_groupings.commercial_term_id,
-    carton_groupings.advance_price,
-    carton_groupings.advance_due_date,
-    carton_groupings.final_price,
-    carton_groupings.final_due_date,
-    carton_groupings.currency,
-    carton_groupings.transport_type,
-    carton_groupings.packed_datetime,
-    carton_groupings.first_event_datetime
+    carton_groupings.cartons,
+    mv_pallet_timeline."STD Cartons" std_cartons
+
 
 FROM 
     carton_groupings
+
+    LEFT JOIN 
+        data_science.mv_pallet_timeline
+        ON carton_groupings.id = mv_pallet_timeline.cg_id
 
     LEFT JOIN 
         commodities 
@@ -63,14 +104,6 @@ FROM
     LEFT JOIN 
         production_regions 
         ON food_business_operators.production_region_id = production_regions.id
-        
-    LEFT JOIN 
-        cg_owners cg_buyers
-        ON carton_groupings.id = cg_buyers.carton_grouping_id AND carton_groupings.buyer_id = cg_buyers.company_id
-    
-    LEFT JOIN 
-        cg_owners cg_sellers
-        ON carton_groupings.id = cg_sellers.carton_grouping_id AND carton_groupings.seller_id = cg_sellers.company_id
 
 WHERE 
-    map_season_year = 2024 AND commodities.commodity_group = 'Citrus' 
+    carton_groupings.map_season_year = 2024 AND mv_pallet_timeline.commodity_group = 'Citrus' 
